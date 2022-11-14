9d01b07a1ca00a959ecda2c0bac14becf07b4232
sweta <swetajaiswal121@gmail.com>
2022-11-14 13:56:20 +0530

    Added log msg

--- src/main/java/com/controller/ControllerClass.java
+++ src/main/java/com/controller/ControllerClass.java
@@ -1,14 +1,20 @@
 package com.controller;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class ControllerClass {
 
+	
+
 	@GetMapping("/dock")
 	 public String test()
 	 {
+		 System.out.println("Within controller class");
 		return "Welcome to Spring Boot Dockerize World";
 	 }
+	
 }
